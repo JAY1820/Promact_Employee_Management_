4e3d62eadfb99b21ea05a2137732689c307c962d
JAY1820 <iamjay1820@gmail.com>
2024-02-22 11:08:31 +0530

    v3- fit and fix code

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,12 @@
 const employeesData = [
   {
     id: 1,
-    firstName: 'Jay',
-    lastName: 'Valaki',
-    birthdate: '2001-12-12',
-    department: 'Mern',
-    experience: '1 years'
-  }
-    
+    firstName: "Jay",
+    lastName: "Valaki",
+    birthdate: "2001-12-12",
+    department: "Mern",
+    experience: "1",
+  },
 ];
 
 export { employeesData };
